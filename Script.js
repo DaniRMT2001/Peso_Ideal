202cc3fc5130c8522cd7ae58bb97c26543eaeef9
roberto daniel marte tejada <danielrmt2001@gmail.com>
2022-02-18 09:51:38 -0400

    posible final

---
+++ Script.js
@@ -0,0 +1,34 @@
+function Datos_person() {
+
+    let persona = {
+        nombre: document.getElementById("Nombre").value,
+        estatura_pies: document.getElementById("Estatura").value,
+        peso: document.getElementById("peso").value,
+    }
+
+    estatura_metro=persona.estatura_pies/3.2808
+    peso_ideal=persona.peso/estatura_metro
+
+    estatura_metro=estatura_metro.toFixed(2)
+    peso_ideal=peso_ideal.toFixed(2)
+
+    let datos = []
+    datos.push(persona)
+    
+
+    let tabla = document.getElementById("Datos");
+    tabla.innerHTML += ""
+    for (let x of datos) {
+        tabla.innerHTML += `
+            <tr>
+            <td>${x.nombre}</td>
+            <td>${x.estatura_pies +' ft'}</td>
+            <td>${estatura_metro +' m'}</td>
+            <td>${x.peso +' Kg'}</td>
+            <td>${peso_ideal +' Kg'}</td>
+            </tr>`
+     }
+
+    
+}
+
